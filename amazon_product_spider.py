6f89e0caee5daef4852ccde13480148f95b519cf
Wisdom Uche Ijika <155220912+Iammrgemini@users.noreply.github.com>
2024-04-12 11:31:06 +0100

    Add files via upload

---
+++ amazon_product_spider.py
@@ -0,0 +1,52 @@
+import json
+import scrapy
+from urllib.parse import urljoin
+import re
+
+class AmazonSearchProductSpider(scrapy.Spider):
+    name = "amazon_product_spider"
+
+    def start_requests(self):
+        keyword_list = ['ipad']
+        for keyword in keyword_list:
+            amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page=1'
+            yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': 1})
+
+    def discover_product_urls(self, response):
+        page = response.meta['page']
+        keyword = response.meta['keyword']
+
+        #Discover Product URLS
+        search_products = response.css("div.s-result-item[data-component-type=s-search-result]")
+        for product in search_products:
+            relative_url = product.css("h2 a::attr(href)").get()
+            product_url = urljoin('https://www.amazon.com/', relative_url).split("?")[0]
+            yield scrapy.Request(url=product_url, callback=self.parse_prodcut_data, meta={'keyword': keyword, 'page': page})
+
+        ## Get All Pages
+        if page == 1:
+            available_pages = response.xpath(
+                '//*[contains(@class, "s-pagination-item")][not(has-class("s-pagination-separator"))]/text()'
+            ).getall()
+
+            last_page = available_pages[-1]
+            for page_num in range(2, int(last_page)):
+                amazon_search_url = f'https://www.amazon.com/s?k={keyword}&page={page_num}'
+                yield scrapy.Request(url=amazon_search_url, callback=self.discover_product_urls, meta={'keyword': keyword, 'page': page_num})
+
+    def parse_prodcut_data(self,response):
+        image_data = json.loads(re.findall(r"colorImages':.*'initial':\s*(\[.+?\])},\n", response.text)[0])
+        variant_data = re.findall(r'dimensionValuesDisplayData"\s*:\s* ({.+?}),\n', response.text)
+        feature_bullets = [bullet.strip() for bullet in response.css("#feature-bullets li ::text").getall()]
+        price = response.css('.a-price span[aria-hidden="true"] ::text').get("")
+        if not price:
+            price = response.css('.a-price .a-offscreen ::text').get("")
+        yield {
+            "name": response.css("#productTitle::text").get("").strip(),
+            "price": price,
+            "stars": response.css("i[data-hook=average-star-rating] ::text").get("").strip(),
+            "rating_count": response.css("div[data-hook=total-review-count] ::text").get("").strip(),
+            "feature_bullets": feature_bullets,
+            "images": image_data,
+            "variant_data": variant_data,
+        }
